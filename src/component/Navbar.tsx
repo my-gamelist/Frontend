4ce85a3a29249baeaff56721f472cf8cc85f260a
Mihir Kadiya <mkadiya@uwo.ca>
2022-10-14 21:35:41 -0400

    added the full game detail UI

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -3,17 +3,17 @@ import Searchbar from "./Searchbar"
 
 export default function Navbar() {
     return (
-        <div className="flex w-screen h-24 bg-white bg-opacity-30 justify-center items-center">
+        <div className="flex w-screen h-16 bg-white bg-opacity-30 justify-center items-center">
             <Searchbar />
-            <div className="flex absolute transition ease-in-out delay-10 right-10 w-28 h-10 bg-white bg-opacity-30 hover:bg-opacity-50 text-white justify-center items-center rounded-full">
+            <div className="flex absolute cursor-pointer transition ease-in-out delay-10 right-10 w-28 h-10 bg-white bg-opacity-30 hover:bg-opacity-50 text-white justify-center items-center rounded-full">
                 <span className="flex absolute left-3 text-white"><FaSignInAlt /></span>
                 <span>Login</span>
             </div>
             <div className="flex absolute left-10 w-28 h-10 text-lg font-bold text-white justify-center items-center">
-                <span>Top Games</span>
+                <span className="cursor-pointer">Top Games</span>
             </div>
             <div className="flex absolute left-44 w-34 h-10 text-lg font-bold text-white justify-center items-center">
-                <span>Popular Games</span>
+                <span className="cursor-pointer">Popular Games</span>
             </div>
         </div>
     )
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -22,38 +22,120 @@ export default function Game() {
             <Navbar />
             <div className="flex flex-row w-full h-screen">
                 {/* This div is for game image and description */}
-                <div className="w-1/2 ml-10 mt-10 mr-10">
+                <div className="w-1/2 ml-10 mr-10">
                     <div className="w-full h-1/2 p-10">
                         <img className="flex relative w-full h-full rounded-2xl" src="https://cdn.akamai.steamstatic.com/steam/apps/730/capsule_616x353.jpg?t=1641233427"></img>
                     </div>
-                    <div className="w-full h-1/2 p-10">
+                    <div className="w-full h-1/2 pl-10 pr-10 pb-10">
                         <div className="flex flex-col relative w-full h-full text-white bg-white bg-opacity-30 rounded-2xl">
-                            <div className="flex relative w-full h-10 border-b-2 border-white justify-center items-center">
+                            <div className="flex relative w-full h-14 border-b-2 border-white justify-center items-center">
                                 <span className="flex absolute left-4 text-lg font-bold">Summary</span>
                             </div>
-                            <div className="flex p-4">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Etiam ligula nulla, ornare et faucibus in, finibus at nibh.
-                                Curabitur tempor fringilla tempor. Nunc est urna,
-                                suscipit vel urna ut, sodales convallis dui.
-                                Vivamus pulvinar, dolor in elementum ultricies,
-                                augue felis porttitor turpis, nec molestie ex felis et lorem.
-                                Donec vitae leo nec mauris placerat fringilla.
-                                Donec semper tempor lorem, sit amet fringilla neque fermentum nec.
-                                Phasellus fermentum pretium mollis.
+                            <div className="flex overflow-auto p-4">
+                                {game?.description}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="w-1/2 ml-10 mt-10 mr-10 bg-yellow-600">
-
+                <div className="flex flex-col w-1/2 mt-10 mr-10">
+                    {/* Game Title */}
+                    <div className="flex relative w-full h-14 bg-white bg-opacity-30 rounded-2xl">
+                        <span className="absolute left-4 text-white text-5xl font-bold">{game?.gameName}</span>
+                    </div>
+                    {/* panels */}
+                    <div className="flex flex-row space-x-4 w-full h-24 mt-6">
+                        {/* Rating panel */}
+                        <div className="flex flex-col relative w-1/5 h-full rounded-2xl bg-white bg-opacity-30 items-center">
+                            <span className="flex relative text-white">Rating</span>
+                            <span className="flex relative text-white text-6xl font-bold">8.6</span>
+                        </div>
+                        {/* Ranking panel */}
+                        <div className="flex flex-col relative w-1/5 h-full rounded-2xl bg-white bg-opacity-30 items-center">
+                            <span className="flex relative text-white">Ranking</span>
+                            <span className="flex relative text-white text-6xl font-bold">18</span>
+                        </div>
+                        {/* Popularity number panel */}
+                        <div className="flex flex-col relative w-1/5 h-full rounded-2xl bg-white bg-opacity-30 items-center">
+                            <span className="flex relative text-white">Popularity</span>
+                            <span className="flex relative text-white text-6xl font-bold">71</span>
+                        </div>
+                        {/* Member panel */}
+                        <div className="flex flex-col relative w-2/5 h-full rounded-2xl bg-white bg-opacity-30 items-center">
+                            <span className="flex relative text-white">Members</span>
+                            <span className="flex relative text-white text-6xl font-bold">284713</span>
+                        </div>
+                    </div>
+                    {/* Additional information panel */}
+                    <div className="flex flex-col space-y-2 relative w-full h-44 mt-6 text-white text-lg bg-white bg-opacity-30 rounded-2xl justify-center">
+                        {/* App id */}
+                        <div className="flex flex-row relative w-full h-8 pl-10 pr-10">
+                            <span className="flex relative w-1/2 h-full bg-white bg-opacity-50 rounded-l-2xl border-r-2 border-l-white border-opacity-30 justify-center items-center">App ID</span>
+                            <span className="flex relative w-1/2 h-full bg-white bg-opacity-50 rounded-r-2xl justify-center items-center">{game?.appId}</span>
+                        </div>
+                        <div className="flex flex-row relative w-full h-8 pl-10 pr-10">
+                            <span className="flex relative w-1/2 h-full bg-white bg-opacity-50 rounded-l-2xl border-r-2 border-l-white border-opacity-30 justify-center items-center">Release Date</span>
+                            <span className="flex relative w-1/2 h-full bg-white bg-opacity-50 rounded-r-2xl justify-center items-center">12/29/2022</span>
+                        </div>
+                        <div className="flex flex-row relative w-full h-8 pl-10 pr-10">
+                            <span className="flex relative w-1/2 h-full bg-white bg-opacity-50 rounded-l-2xl border-r-2 border-l-white border-opacity-30 justify-center items-center">Developers</span>
+                            <span className="flex relative w-1/2 h-full bg-white bg-opacity-50 rounded-r-2xl justify-center items-center">{game?.developers}</span>
+                        </div>
+                        <div className="flex flex-row relative w-full h-8 pl-10 pr-10">
+                            <span className="flex relative w-1/2 h-full bg-white bg-opacity-50 rounded-l-2xl border-r-2 border-l-white border-opacity-30 justify-center items-center">Publishers</span>
+                            <span className="flex relative w-1/2 h-full bg-white bg-opacity-50 rounded-r-2xl justify-center items-center">{game?.publishers}</span>
+                        </div>
+                    </div>
+                    {/* User reviews title */}
+                    <div className="flex relative w-full h-14 mt-6 bg-white bg-opacity-30 rounded-t-2xl items-center">
+                        <span className="flex relative left-4 text-white text-3xl font-bold">User Reviews</span>
+                    </div>
+                    {/* User reviews list */}
+                    <div className="flex flex-col overflow-auto relative w-full h-64 text-white bg-white bg-opacity-30 rounded-b-2xl">
+                        <div className="flex flex-row relative w-full h-full p-5">
+                            <div className="flex flex-col space-y-8 relative w-1/4 h-full bg-white bg-opacity-50 rounded-l-2xl justify-center items-center">
+                                <span>User</span>
+                                <span>Review date</span>
+                                <span>User rating</span>
+                                <span>Open Review</span>
+                            </div>
+                            <div className="flex relative w-3/4 h-full p-4 bg-white bg-opacity-20 rounded-r-2xl border-l-2 border-l-white border-opacity-30 justify-center items-center">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Etiam ligula nulla, ornare et faucibus in, finibus at nibh.
+                                    Curabitur tempor fringilla tempor. Nunc est urna,
+                                    suscipit vel urna ut, sodales convallis dui.
+                                    Vivamus pulvinar, dolor in elementum ultricies,
+                                    augue felis porttitor turpis, nec molestie ex felis et lorem.
+                                    Donec vitae leo nec mauris placerat fringilla.
+                                    Donec semper tempor lorem, sit amet fringilla neque fermentum nec.
+                                    Phasellus fermentum pretium mollis.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row relative w-full h-full p-5">
+                            <div className="flex flex-col space-y-8 relative w-1/4 h-full bg-white bg-opacity-50 rounded-l-2xl justify-center items-center">
+                                <span>User</span>
+                                <span>Review date</span>
+                                <span>User rating</span>
+                                <span>Open Review</span>
+                            </div>
+                            <div className="flex relative w-3/4 h-full p-4 bg-white bg-opacity-20 rounded-r-2xl border-l-2 border-l-white border-opacity-30 justify-center items-center">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Etiam ligula nulla, ornare et faucibus in, finibus at nibh.
+                                    Curabitur tempor fringilla tempor. Nunc est urna,
+                                    suscipit vel urna ut, sodales convallis dui.
+                                    Vivamus pulvinar, dolor in elementum ultricies,
+                                    augue felis porttitor turpis, nec molestie ex felis et lorem.
+                                    Donec vitae leo nec mauris placerat fringilla.
+                                    Donec semper tempor lorem, sit amet fringilla neque fermentum nec.
+                                    Phasellus fermentum pretium mollis.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     )
-
-    // const res = getGameDetail(params['gameId'])
-    //     .then((data) => {
-    //         setGame(data)
-    //     });
 }
